383bb20467ff08ebc04048c1857418e9830aad3e
GiuTP <giuliannopinheirotavares@gmail.com>
2026-09-04 12:51:37 -0300

    feat: add models defined at db

---
+++ frontend/types/index.ts
@@ -0,0 +1,11 @@
+export interface Amigurumi {
+    id: string;
+    name: string;
+    image_url: string;
+    difficulty: number;
+    satisfaction: number;
+    time_spent_hours: number;
+    materials: string[];
+    story: string;
+    created_at: string;
+}
